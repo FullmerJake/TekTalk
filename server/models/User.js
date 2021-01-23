96d0ae0b4b0b10b1b84f128046056b6cce0a6ece
sfoutz0205 <68661461+sfoutz0205@users.noreply.github.com>
2021-01-22 19:13:35 -0700

    add password validation

--- server/models/User.js
+++ server/models/User.js
@@ -30,8 +30,18 @@ const userSchema = new Schema(
   }
 );
 
-// validate password when logging in 
-userSchema.methods.isCorrectPassword = async function (password) {
+// set up pre-save middleware to create password
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// validate incoming password against hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
   return bcrypt.compare(password, this.password);
 };
 
