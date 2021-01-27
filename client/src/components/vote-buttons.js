cdbea078b56a86db3711176f5eb016e42f09e0a9
Jake Fullmer <w.jake.fullmer@gmail.com>
2021-01-27 02:45:53 -0700

    updated voting to check localStorage if already voted on post

--- client/src/components/vote-buttons.js
+++ client/src/components/vote-buttons.js
@@ -1,10 +1,42 @@
 import { IconButton, Text, VStack } from '@chakra-ui/core';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { FiArrowDown, FiArrowUp } from 'react-icons/fi';
 import db from '../lib/firebase';
 
+
+// VoteButtons component responsible for rendering an upvote and downvote button
 const VoteButtons = ({ post }) => {
+    const [isVoting, setVoting] = useState(false);
+    const [votedPosts, setVotedPosts] = useState([]);
+
+    useEffect(() => {
+        // Fetch the previously voted items from localStorage. 
+        const votesFromLocalStorage = localStorage.getItem("votes") || [];
+        let previousVotes = [];
+
+        // Parse the value of the item from localStorage.
+        // If value of the items isn't an array, then JS will throw an error
+        try {previousVotes = JSON.parse(votesFromLocalStorage);}
+        catch(error){ console.log(error);}
+
+        setVotedPosts(previousVotes);
+    }, []);
+
+    const handleDisablingOfVotes = (postId) => {
+        // Responsible for disabling voting once a user has voted. 
+        const previousVotes = votedPosts;
+        previousVotes.push(postId);
+
+        setVotedPosts(previousVotes);
+
+        // Update the voted items from localStorage
+        localStorage.setItem("votes", JSON.stringify(votedPosts));
+    };
+
+    // when a user clicks on either button, call this function. 
     const handleClick = async (type) => {
+        setVoting(true);
+
         //Do calculations to save the vote.
         let upVotesCount = post.upVotesCount;
         let downVotesCount = post.downVotesCount;
@@ -25,6 +57,20 @@ const VoteButtons = ({ post }) => {
             createdAt: post.createdAt,
             updatedAt: date.toUTCString()
         });
+
+        // Disable voting button once voting is successful. 
+        handleDisablingOfVotes(post.id);
+
+        setVoting(true);
+    };
+
+    const checkIfPostIsAlreadyVoted = () => {
+        if(votedPosts.indexOf(post.id) > -1) {
+            return true;
+        }
+        else {
+            return false;
+        }
     };
 
     return (
@@ -36,6 +82,8 @@ const VoteButtons = ({ post }) => {
                     aria-label="Upvote"
                     icon={<FiArrowUp />}
                     onClick={() => handleClick("upvote")}
+                    isLoading={isVoting}
+                    isDisabled={checkIfPostIsAlreadyVoted}
                 >
                     <Text bg="gray.100" rounded="md" w="100%" p={1}>
                         {post.upVotesCount}
@@ -49,6 +97,8 @@ const VoteButtons = ({ post }) => {
                     aria-label="Downvote"
                     icon={<FiArrowDown />}
                     onClick={() => handleClick("downvote")}
+                    isLoading={isVoting}
+                    isDisabled={checkIfPostIsAlreadyVoted}
                 >
                     <Text bg="gray.100" rounded="md" w="100%" p={1}>
                         {post.downVotesCount}
